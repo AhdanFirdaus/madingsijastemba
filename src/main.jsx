897a2f6f87e053d420c3c8481fd17fbb144b999b
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-07 18:04:54 +0700

    chore: install and setup react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1 className='flex items-center justify-center min-h-screen text-2xl font-bold text-center'>Hello World</h1>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </StrictMode>,
 )
