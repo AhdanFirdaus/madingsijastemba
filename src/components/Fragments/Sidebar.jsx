e944f871da20cb09db2eb556a732b9d573504a71
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-08 13:39:34 +0700

    style: sidebar background opacity

--- src/components/Fragments/Sidebar.jsx
+++ src/components/Fragments/Sidebar.jsx
@@ -13,7 +13,7 @@ function Sidebar({ isOpen, toggleSidebar }) {
     <>
       {/* Overlay for mobile */}
       <div 
-        className={`fixed inset-0 bg-black bg-opacity-50 z-40 transition-opacity duration-300 md:hidden ${
+        className={`fixed inset-0 bg-slate-950/50 z-40 transition-opacity duration-300 md:hidden ${
           isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
         }`}
         onClick={toggleSidebar}
