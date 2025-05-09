4d020f1c5e23dcc145fee84c0dd412859ad54fbd
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-09 20:32:18 +0700

    feat: add value prop to input element

--- src/components/Elements/Input.jsx
+++ src/components/Elements/Input.jsx
@@ -7,6 +7,7 @@ const Input = ({
   required = true,
   placeholder,
   onChange,
+  value,
 }) => {
   return (
     <div>
@@ -22,6 +23,7 @@ const Input = ({
           autoComplete={autoComplete}
           placeholder={placeholder}
           onChange={onChange}
+          value={value}
           className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-rose-500 sm:text-sm/6"
         />
       </div>
