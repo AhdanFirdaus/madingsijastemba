dc9bf814d92638d4a007330ee150f0660e5d297a
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-12 21:51:06 +0700

    wip: add Home page (temporary)

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Fragments/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="pt-32 container mx-auto px-10">
+        {/* konten utama */}
+      </div>
+    </>
+  );
+}
